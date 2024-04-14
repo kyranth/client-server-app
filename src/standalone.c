4a7949780827543c17001ce8932ef47c8c9ef37b
Kyranth <50931322+Kyranth@users.noreply.github.com>
2024-04-14 05:58:20 +0000

    Updated standalone driver

--- src/standalone.c
+++ src/standalone.c
@@ -3,18 +3,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <client.h>
-#include <server.h>
+#include "client.h"
+#include "server.h"
+
+/**
+ * @brief Get the time
+ *
+ * @return double time
+ */
+double get_time()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double)tv.tv_sec * 1000.0 + (double)tv.tv_usec / 1000.0;
+}
 
 int main()
 {
-    Client client;
-    Server server;
+    Client *client;
+    Server *server;
 
-    client.sendPackets();
-    server.receivePackets();
+    /** Initialize the client and server */
+    start_client(client);
+    start_server(server);
 
-    double time_diff_low = server.getTimeDifference();
+    /** Send the first packet train */
+    int send = send_packet(client, "Hello");
+    if (send < 0)
+    {
+        printf("Error: Counldn't send packet");
+    }
+
+    /** Receive the packets */
+    int recv = receive_packet(server);
+    if (recv < 0)
+    {
+        printf("Error: Counldn't send packet");
+    }
+
+    double time_diff_low = server->end_time - server->start_time;
 
     printf("Time difference for low compression: %.2f ms\n", time_diff_low);
 

84ac689b3533be2133f7e675b47d803a52165740
Kyranth <50931322+Kyranth@users.noreply.github.com>
2024-04-15 05:39:16 +0000

    Updated Server

--- src/server.h
+++ src/server.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -13,8 +12,6 @@
 
 #define SERVER_PORT 12345
 #define PACKET_SIZE 1024
-#define NUM_PACKETS 100
-#define THRESHOLD 100 // Threshold in milliseconds
 
 /**
  * @brief Represents the server side of the network compression detection application
@@ -35,7 +32,7 @@ typedef struct
     char buffer[PACKET_SIZE];
 
     /** Time variables */
-    double start_time, end_time;
+    char start_time[20], end_time[20];
 } Server;
 
 /**
@@ -47,7 +44,7 @@ void start_server(Server *server)
 {
     if (server == NULL)
     {
-        perror("Error initializing Server: Memory allocation failed");
+        perror("Error initializing Server");
     }
 
     // Create UDP socket
@@ -58,11 +55,28 @@ void start_server(Server *server)
         exit(EXIT_FAILURE);
     }
 
-    // Initialize server address struct
+    // Set server address to 0s
     memset(&server->servaddr, 0, sizeof(server->servaddr));
+
+    // IPv4
     server->servaddr.sin_family = AF_INET;
-    server->servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Default interface
+
+    // Server port
     server->servaddr.sin_port = htons(SERVER_PORT);
+
+    // Set to default interface
+    server->servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind
+    int rc = bind(server->sockfd, (const struct sockaddr *)&server->servaddr,
+                  sizeof(server->servaddr));
+
+    if (rc == -1)
+    {
+        perror("Failed to bind");
+        close(server->sockfd);
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
@@ -73,38 +87,16 @@ void start_server(Server *server)
  */
 int receive_packet(Server *server)
 {
-    int rc = bind(server->sockfd, (const struct sockaddr *)&server->servaddr,
-                  sizeof(server->servaddr));
-
-    if (rc == -1)
-    {
-        perror("Failed to bind");
-        close(server->sockfd);
-        exit(EXIT_FAILURE);
-    }
     server->clilen = 0;
+
+    // Receive
     int n = recvfrom(server->sockfd, server->buffer, PACKET_SIZE, 0,
                      (struct sockaddr *)&server->servaddr, &server->clilen);
 
     server->buffer[n] = '\n';
     printf("%s", server->buffer);
+    printf("Msg recieved at %s\n", server->end_time);
     return 0;
-}
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(EXIT_FAILURE);
-}
-
-/**
- * @brief Clean up the Server object. Destructor method for cleaning up resources used by the server.
- *
- */
-void close(Server *server)
-{
-    close(server->sockfd);
-    free(server);
 }
 
 #endif

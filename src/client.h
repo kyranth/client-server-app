30412b6d68f985f6d37e957d90c35df90bbe8445
Kyranth <50931322+Kyranth@users.noreply.github.com>
2024-04-15 05:38:47 +0000

    Updated client

--- src/client.h
+++ src/client.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -13,7 +12,6 @@
 
 #define SERVER_PORT 12345
 #define PACKET_SIZE 1024
-#define NUM_PACKETS 100
 
 /**
  * @brief Represents the client side of the network compression detection application
@@ -39,7 +37,7 @@ void start_client(Client *client)
 {
     if (client == NULL)
     {
-        error("Error initializing Server: Memory allocation failed");
+        perror("Error initializing Client");
     }
 
     // Create UDP socket as IPv4
@@ -50,14 +48,17 @@ void start_client(Client *client)
         exit(EXIT_FAILURE);
     }
 
-    // Initialize server address struct
+    // Set the server address to all 0s
     memset(&client->serv_addr, 0, sizeof(client->serv_addr));
+
+    // IPv4 protocol
     client->serv_addr.sin_family = AF_INET;
+
+    // Setting server port
     client->serv_addr.sin_port = htons(SERVER_PORT);
-    if (inet_pton(AF_INET, "127.0.0.1", &client->serv_addr.sin_addr) <= 0)
-    {
-        perror("Invalid address");
-    }
+
+    // Send to deafault interface "0.0.0.0"
+    client->serv_addr.sin_addr.s_addr = INADDR_ANY;
 }
 
 /**
@@ -74,33 +75,11 @@ int send_packet(Client *client, char *msg)
 
     if (client->len < 0)
     {
-        error("Error sending second packet train");
+        perror("Error: failed to send");
         return -1;
     }
 
     return 0;
-}
-
-/**
- * @brief Generates the given error message
- *
- * @param msg error message
- */
-void error(const char *msg)
-{
-    perror(msg);
-    exit(EXIT_FAILURE);
-}
-
-/**
- * @brief Closes the client object
- *
- * @param client struct
- */
-void close(Client *client)
-{
-    close(client->sockfd);
-    free(client);
 }
 
 #endif
